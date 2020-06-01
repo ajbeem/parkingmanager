aa9d50585ee3444d892d470bb868c49111a539e1
IDS. Alfredo jimenez <fred.jm.74@gmail.com>
2020-06-01 15:24:17 -0500

    Update IfaceSingletonConect.java
    Iface update

--- src/com/pcentaury/parkingmanager/services/IfaceSingletonConect.java
+++ src/com/pcentaury/parkingmanager/services/IfaceSingletonConect.java
@@ -6,7 +6,7 @@ public interface IfaceSingletonConect {
 	String urlHost ="jdbc:mysql://185.201.11.149:3306/u538442363_aGHVt";
 	String driver="com.mysql.jdbc.Driver";
 	String userName="u538442363_9QoPD";
-	String userPwd= "ehrmt*53526";		
+	String userPwd= "server1root";		
 	
 	Connection getConnection();
 	void closeConnection();
